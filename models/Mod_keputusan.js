856c49435612a415fed5b191eaa913a2a61d5e92
barleyIMakbar <barley.barley1199@gmail.com>
2020-07-06 17:07:05 +0700

    next->tampilkan soal test

--- models/Mod_keputusan.js
+++ models/Mod_keputusan.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser');
-const e = require('express');
 const router = express.Router()
 const conn = require("../config").pool;
 // const uuid = require('uuid')
