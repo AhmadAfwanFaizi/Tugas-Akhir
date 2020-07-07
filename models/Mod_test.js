f5ca1a5ffbf5a6a12bf61a51e1768282dc4681ff
barleyIMakbar <barley.barley1199@gmail.com>
2020-07-07 16:16:57 +0700

    masih di test-itung hasil

--- models/Mod_test.js
+++ models/Mod_test.js
@@ -30,4 +30,55 @@ router.post('/', (req, res) => {
     })
 })
 
+router.post('/getSoal', (req, res) => {
+    var data = "SELECT * from soal_tes"
+    conn.query(data, (err, result) => {
+        if (err) {
+            res.json({
+                code: 500,
+                data: null
+            });
+        } else {
+            res.json({
+                code: 200,
+                data: result
+            });
+        }
+    })
+})
+
+router.post('/add', (req, res) => {
+    var jawaban = req.body.v_jawaban
+    var id = req.body.v_id
+    var baris = req.body.v_row
+    var sess = req.body.v_sess
+    var pg1 = 0
+    var pg2 = 0
+    var pg3 = 0
+    var pg4 = 0
+
+    for (var i = 0, j = baris; i < j; i++) {
+        var jwb = jawaban[i]
+        if (jwb == 'A') {
+            pg1++
+        } else if (jwb == 'B') {
+            pg2++
+        } else if (jwb == 'C') {
+            pg3++
+        } else {
+            pg6++
+        }
+    }
+    console.log("JAWABAN 1 = " + pg1)
+    console.log("JAWABAN 2 = " + pg2)
+    console.log("JAWABAN 3 = " + pg3)
+    console.log("JAWABAN 4 = " + pg4)
+    console.log("JAWABAN 5 = " + pg5)
+    console.log("JAWABAN 6 = " + pg6)
+    res.json({
+        code: 200,
+        message: "Succes insert data!!"
+    })
+})
+
 module.exports = router
